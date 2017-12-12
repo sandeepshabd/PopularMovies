4afe910f17ba24ea59123574abbecdada25d6679
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-11 21:08:45 -0500

    adding skeleton code for android app

---
+++ app/src/main/java/com/sandeepshabd/popularmovies/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.sandeepshabd.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
