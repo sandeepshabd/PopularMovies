695f677591d223ae30b1b13683eee019366d1262
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-12 22:21:20 -0500

    addding supporting activities that has to be called after splash activity

---
+++ app/src/main/java/com/sandeepshabd/popularmovies/activity/ErrorActivity.java
@@ -0,0 +1,22 @@
+package com.sandeepshabd.popularmovies.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.sandeepshabd.popularmovies.R;
+
+/*
+* This activity will show error screen.
+* */
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+
+        TextView errorMessage = findViewById(R.id.textView2);
+        errorMessage.setText("An error has occurred while retrieving the data. ");
+    }
+}
