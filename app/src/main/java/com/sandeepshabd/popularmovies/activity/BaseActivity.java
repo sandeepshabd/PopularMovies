df597cdcd7cf9faf4ee9b7c98593cec4093d053d
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-14 02:24:45 -0500

    changing colors and adding toolbar buttons

--- app/src/main/java/com/sandeepshabd/popularmovies/activity/BaseActivity.java
+++ app/src/main/java/com/sandeepshabd/popularmovies/activity/BaseActivity.java
@@ -13,20 +13,22 @@ public abstract class BaseActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         hideNavigationBar();
+        hideSystemUI();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        hideNavigationBar();
+        hideSystemUI();
     }
 
-
-    private void hideNavigationBar(){
-        getWindow().
-                getDecorView().
-                setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        // remove the following flag for version < API 19
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
     }
 }
--- app/src/main/java/com/sandeepshabd/popularmovies/activity/MovieListingActivity.java
+++ app/src/main/java/com/sandeepshabd/popularmovies/activity/MovieListingActivity.java
@@ -1,8 +1,15 @@
 package com.sandeepshabd.popularmovies.activity;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 import com.bumptech.glide.Glide;
 import com.sandeepshabd.popularmovies.R;
@@ -20,16 +27,34 @@ public class MovieListingActivity extends BaseActivity {
 
     private static final String TAG = MovieListingActivity.class.getSimpleName();
     public static final String MOVIE_DATA = "MOVIE_DATA";
+    private Context context ;
     private MovieListingPresenter movieListingPresenter;
     private int pageCount = 1; //initialize the page count to 1.
     private RecyclerView movieListingRecyclerView;
     private MovieResponse movieResponse;
+    private Toolbar toolbar;
+    private ImageButton aboutInfoButton;
+
 
     @DebugLog
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_movie_listing);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setTitle(R.string.popular_movies);
+
+        aboutInfoButton = findViewById(R.id.showAbout);
+        aboutInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showAboutScreen = new Intent(context, AboutActivity.class);
+                context.startActivity(showAboutScreen);
+            }
+        });
         String movieData = getIntent().getStringExtra(MOVIE_DATA);
         movieListingPresenter = new MovieListingPresenter(this);
         movieResponse = movieListingPresenter.parseMovieData(movieData);
