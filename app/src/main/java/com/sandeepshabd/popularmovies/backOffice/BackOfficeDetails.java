2e0db106bcdd8e9b1c44d8a9ccfd91460e80d2fc
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-13 22:32:36 -0500

    adding code to support api key and correcting the URl

--- app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
+++ app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
@@ -1,5 +1,7 @@
 package com.sandeepshabd.popularmovies.backOffice;
 
+import android.text.TextUtils;
+
 import hugo.weaving.DebugLog;
 
 /**
@@ -12,19 +14,23 @@ public class BackOfficeDetails {
     //mock the BO.
     final private static String BASE_URL = "https://api.themoviedb.org/3";
     final private static String BASE_IMG_PATH = "https://image.tmdb.org/t/p/w500";
-    final private static String API_KEY = "";
+    final private static String API_KEY = ""; //input your key here.
 
 
     // The method returns the uri for popular movie
     @DebugLog
     public static String getPopularMoviesURL(int page) {
+        if(TextUtils.isEmpty(API_KEY)){
+            //enforcing the developer to input the API key.
+            throw new RuntimeException("API KEY cannot be empty.");
+        }
         return BASE_URL + "/movie/popular?api_key=" + API_KEY + "&language=en-US&page=" + page;
     }
 
     @DebugLog
     public static String getImagePathURL(String uri){
         //eg. https://image.tmdb.org/t/p/w500/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg
-        return BASE_IMG_PATH + uri.substring(1);
+        return BASE_IMG_PATH + uri.substring(0);
     }
 
 
