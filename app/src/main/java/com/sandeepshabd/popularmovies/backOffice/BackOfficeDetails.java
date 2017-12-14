8f3a24e3a2e36055566c2b2a9058d8060cb41c5d
Sandeep Shabd <sandeep.shabd@gm.com>
2017-12-13 19:29:17 -0500

    adding dependencies and adding static method to fetch image

--- app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
+++ app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
@@ -1,5 +1,7 @@
 package com.sandeepshabd.popularmovies.backOffice;
 
+import hugo.weaving.DebugLog;
+
 /**
  * The class will provide the details for back office URLS.
  */
@@ -9,15 +11,21 @@ public class BackOfficeDetails {
     //Provides the base url. It can be pointed to localhost if a fake server is used to
     //mock the BO.
     final private static String BASE_URL = "https://api.themoviedb.org/3";
-    final private static String API_KEY = "";
+    final private static String API_KEY = "71adffcb631e5befc6b880ca18d29e09";
+    final private static String BASE_IMG_PATH = "https://image.tmdb.org/t/p/w500";
 
 
     // The method returns the uri for popular movie
-    public static String getPopularMoviesURL( int page) {
+    @DebugLog
+    public static String getPopularMoviesURL(int page) {
         return BASE_URL + "/movie/popular?api_key=" + API_KEY + "&language=en-US&page=" + page;
     }
 
-
+    @DebugLog
+    public static String getImagePathURL(String uri){
+        //eg. https://image.tmdb.org/t/p/w500/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg
+        return BASE_IMG_PATH + uri.substring(1);
+    }
 
 
 }
