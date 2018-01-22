e6eeb64058c772c0cd0dc47d9c966eeb70af79d2
Sandeep Shabd <sandeep.shabd@gm.com>
2018-01-22 15:22:44 -0500

    adding keys

--- app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
+++ app/src/main/java/com/sandeepshabd/popularmovies/backOffice/BackOfficeDetails.java
@@ -13,14 +13,14 @@ public class BackOfficeDetails {
     //Provides the base url. It can be pointed to localhost if a fake server is used to
     //mock the BO.
     //open m0vie details
-    final private static String API_KEY_FOR_OPENMOVIE = ""; //input your key here.
+    final private static String API_KEY_FOR_OPENMOVIE = "71adffcb631e5befc6b880ca18d29e09"; //input your key here.
     final private static String BASE_IMG_PATH = "https://image.tmdb.org/t/p/w500";
     final private static String BASE_URL_FOR_OPENMOVIE = "https://api.themoviedb.org/3";
     final private static String POPULAR_URL_FOR_OPENMOVIE = "/movie/popular?api_key=";
     final private static String NOWPLAYING_URL_FOR_OPENMOVIE = "/movie/now_playing?api_key=";
 
     //http://developer.tmsapi.com/
-    final private static String API_KEY_FOR_TMS = ""; //input your key here.
+    final private static String API_KEY_FOR_TMS = "qyg6bv47fvaznqppw4544nq2"; //input your key here.
     final private static String BASE_URL_FOR_MS = "http://data.tmsapi.com";
     final private static String THEATERS_URL_FOR_MS = "/v1.1/movies/showings?api_key=";
 
@@ -51,7 +51,7 @@ public class BackOfficeDetails {
             //enforcing the developer to input the API key.
             throw new RuntimeException("API KEY cannot be empty.");
         }
-        return BASE_URL_FOR_MS + THEATERS_URL_FOR_MS + API_KEY_FOR_TMS + "&startDate="+date+"&lng=="+lon;
+        return BASE_URL_FOR_MS + THEATERS_URL_FOR_MS + API_KEY_FOR_TMS + "&startDate="+date+"&lng=\""+lon+"\"&lat=\""+lat+"\"";
     }
 
     @DebugLog
